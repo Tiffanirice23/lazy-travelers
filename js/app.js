c8e6b943752160ef86e097f5494e9f4ab6598885
Tiffani Rice <tiffanirice414@gmail.com>
2023-04-27 11:52:38 -0700

    dev push

--- js/app.js
+++ js/app.js
@@ -24,31 +24,33 @@ let vacationPicThree = document.getElementById('vacationPicThree');
 
 // slashes break deploument on github pages
 function Vacation(name, fileName, fileExtension = 'png') {
-    this.name = name;
-    this.fileName = fileName;
-    this.src = `img/${fileName}.${fileExtension}`;
-    this.votes = 0;
+  this.name = name;
+  this.fileName = fileName;
+  this.src = `img/${fileName}.${fileExtension}`;
+  this.votes = 0;
 }
 
 function createContinent() {
-    // 24 instances
-    const continentArray = ['aztec', 'california', 'candyland', 'ecuador', 'everest', 'france', 'germany', 'harbor', 'italy', 'lithuania', 'mayan', 'mexico', 'norway', 'paris', 'rome', 'sicily', 'switzerland', 'thailand', 'Travel1', 'Travel2', 'Travel3', 'Travel4', 'Travel5', 'viet'];
+  // 24 instances
+  const continentArray = ['aztec', 'california', 'candyland', 'ecuador', 'everest', 'france', 'germany', 'harbor', 'italy', 'lithuania', 'mayan', 'mexico', 'norway', 'paris', 'rome', 'sicily', 'switzerland', 'thailand', 'Travel1', 'Travel2', 'Travel3', 'Travel4', 'Travel5', 'viet'];
 
-    // JPEG: aztec, thailand, viet
+  // JPEG: aztec, thailand, viet
 
-    // PNG: california, ecuador, everest, france, germany,  harbor, italy, lithuania, mayan, mexico, norway, paris, rome, sicily, switzerland
+  // PNG: california, ecuador, everest, france, germany,  harbor, italy, lithuania, mayan, mexico, norway, paris, rome, sicily, switzerland
 
-    // JPG: candyland, Travel 1-5, 
+  // JPG: candyland, Travel 1-5, 
 
-    for (let i = 0; i < continentArray.length; i++) {
-        if (['aztec', 'thailand', 'viet'].includes(continentArray[i])) {
-            travelArray.push(new Vacation(continentArray[i], continentArray[i], 'jpeg'))
+  for (let i = 0; i < continentArray.length; i++) {
+    if (['aztec', 'thailand', 'viet'].includes(continentArray[i])) {
+      travelArray.push(new Vacation(continentArray[i], continentArray[i], 'jpeg'))
     } else if (['candyland', 'Travel1', 'Travel2', 'Travel3', 'Travel4', 'Travel5'].includes(continentArray[i])) {
-            travelArray.push(new Vacation(continentArray[i], continentArray[i], 'jpg'))
-        } else { travelArray.push(new Vacation
-            (continentArray[i], continentArray[i]))}
+      travelArray.push(new Vacation(continentArray[i], continentArray[i], 'jpg'))
+    } else {
+      travelArray.push(new Vacation
+        (continentArray[i], continentArray[i]))
     }
-    console.log(travelArray)
+  }
+  console.log(travelArray)
 }
 createContinent();
 
@@ -64,68 +66,68 @@ createContinent();
 
 
 function randomNumberGenerator() {
-    return Math.floor(Math.random() * travelArray.length);
+  return Math.floor(Math.random() * travelArray.length);
 }
 
 function renderVacation() {
-    while (randomArray.length < 4) {
-        let randomNum = randomNumberGenerator();
-        if (!randomArray.includes(randomNum)) {
-            randomArray.push(randomNum);
-        }
+  while (randomArray.length < 4) {
+    let randomNum = randomNumberGenerator();
+    if (!randomArray.includes(randomNum)) {
+      randomArray.push(randomNum);
     }
+  }
 
-    let imgOneIndex = randomArray.shift();
-    let imgTwoIndex = randomArray.shift();
-    let imgThreeIndex = randomArray.shift();
+  let imgOneIndex = randomArray.shift();
+  let imgTwoIndex = randomArray.shift();
+  let imgThreeIndex = randomArray.shift();
 
-    vacationPicOne.src = travelArray[imgOneIndex].src;
-    vacationPicOne.alt = travelArray[imgOneIndex].name;
-    vacationPicTwo.src = travelArray[imgTwoIndex].src;
-    vacationPicTwo.alt = travelArray[imgTwoIndex].name;
-    vacationPicThree.src = travelArray[imgThreeIndex].src;
-    vacationPicThree.alt = travelArray[imgThreeIndex].name;
+  vacationPicOne.src = travelArray[imgOneIndex].src;
+  vacationPicOne.alt = travelArray[imgOneIndex].name;
+  vacationPicTwo.src = travelArray[imgTwoIndex].src;
+  vacationPicTwo.alt = travelArray[imgTwoIndex].name;
+  vacationPicThree.src = travelArray[imgThreeIndex].src;
+  vacationPicThree.alt = travelArray[imgThreeIndex].name;
 }
 
 // create event.handler
 function handleTravelClick(event) {
-    console.log(travelArray)
-    counter++;
-    console.log(event.target.alt);
-    let clickedTravel = event.target.alt;
-    // find a the duck instance in the duck array whose name property equals the clickedTravel value.
-    for (let i = 0; i < travelArray.length; i++) {
-        if (clickedTravel === travelArray[i].name) {
-            travelArray[i].votes++;
-            console.log(travelArray)
-        }
+  console.log(travelArray)
+  counter++;
+  console.log(event.target.alt);
+  let clickedTravel = event.target.alt;
+  // find a the duck instance in the duck array whose name property equals the clickedTravel value.
+  for (let i = 0; i < travelArray.length; i++) {
+    if (clickedTravel === travelArray[i].name) {
+      travelArray[i].votes++;
+      console.log(travelArray)
     }
+  }
 
-    // check to see if the round has ended
-    if (counter < maxCounter) {
-        // the round can continue
-        renderVacation();
-    } else {
-        // this removes the event listener in order to prevent continued voting.
-        myContainer.removeEventListener('click', handleTravelClick);
-        // make the button clickable
-        viewResultsBtn.addEventListener('click', viewResults);
-        // stop the game and render the results
-    }
+  // check to see if the round has ended
+  if (counter < maxCounter) {
+    // the round can continue
+    renderVacation();
+  } else {
+    // this removes the event listener in order to prevent continued voting.
+    myContainer.removeEventListener('click', handleTravelClick);
+    // make the button clickable
+    viewResultsBtn.addEventListener('click', viewResults);
+    // stop the game and render the results
+  }
 }
 
 // function selectRandomTravelNumber() {
-    //   let randomNum;
-    //   do {
-        //     randomNum = Math.floor(Math.random() * travelArray.length);
-        //   } while (travelArray.includes(randomNum));
-        //   return randomNum;
-        // }
-        // function selectRandomTravelNumber() {
-            //     return Math.floor(Math.random() * travelArray.length);
-            // }
-            // When the very first time the code loads what is the value of the array? Empty array, not Nothing.
-            
-            
-            renderVacation();
-            myContainer.addEventListener('click', handleTravelClick);
+//   let randomNum;
+//   do {
+//     randomNum = Math.floor(Math.random() * travelArray.length);
+//   } while (travelArray.includes(randomNum));
+//   return randomNum;
+// }
+// function selectRandomTravelNumber() {
+//     return Math.floor(Math.random() * travelArray.length);
+// }
+// When the very first time the code loads what is the value of the array? Empty array, not Nothing.
+
+
+renderVacation();
+myContainer.addEventListener('click', handleTravelClick);
