7f0bd6c8eb35469f3cb2d2a17b0c7262bae0d55b
Tiffani Rice <tiffanirice414@gmail.com>
2023-04-26 17:10:34 -0700

    Working Images

--- js/app.js
+++ js/app.js
@@ -5,49 +5,94 @@ let randomArray = [];
 
 let counter = 0;
 
-// Object Constructor
+let myContainer = document.getElementById('#imgContainer');
+let vacationPicOne = document.getElementById('vacationPicOne');
+let vacationPicTwo = document.getElementById('vacationPicTwo');
+let vacationPicThree = document.getElementById('vacationPicThree');
+
 function Vacation(name, fileName, fileExtension = 'jpg') {
     this.name = name;
     this.fileName = fileName;
     this.src = `./img/${this.fileName}.${fileExtension}`;
     this.votes = 0;
+
+    // this.render = function() {
+    //   let container = document.querySelector('#imgContainer');
+    //   let image1 = document.getElementById('#imgContainer');
+
+
+
+    //   let img = document.createElement('img');
+    //   img.src = this.src;
+    //   container.append(img);
+    // }
 }
 
 function makeTravelArray() {
-    let Travel1 = new Vacation('ireland', 'Travel1');
-    let Travel2 = new Vacation('switzerland', 'Travel2');
-    let Travel3 = new Vacation('mexico', 'Travel3');
-    let Travel4 = new Vacation('zimbabwa', 'Travel4');
-    let Travel5 = new Vacation('cockney', 'Travel5');
+    let travel1 = new Vacation('ireland', 'travel1');
+    let travel2 = new Vacation('switzerland', 'travel2');
+    let travel3 = new Vacation('mexico', 'travel3');
+    let travel4 = new Vacation('zimbabwa', 'travel4');
+    let travel5 = new Vacation('cockney', 'travel5');
 
-    travelArray.push(Travel1, Travel2, Travel3, Travel4, Travel5);
+    travelArray.push(travel1, travel2, travel3, travel4, travel5);
+}
+// function renderVacation(img1, img2, img3) {
+//   let container = document.querySelector('#imgContainer');
+//   container.innerHTML = '';
+//   for (let i = 0; i < travelArray.length; i++) {
+//     // console.log('inside of the for loop');
+//     let name = travelArray[i].name;
+//     // console.log(name);
+//     if (name === img1 || name === img2 || name === img3) {
+//       console.log('inside of the if statement');
+//       travelArray[i].render();
+//       // this.render(travelArray[i]);
+//     }
+//   }
+// }
+function randomNumberGenerator () {
+  return Math.floor(Math.random() * travelArray.length);
 }
 
-
-
-let myContainer = document.querySelector('#imgContainer');
-let Travel1 = document.querySelector('#imgContainer img:first-child');
-let Travel2 = document.querySelector('#imgContainer img:nth-child(2)');
-let Travel3 = document.querySelector('#imgContainer img:nth-child(3)');
-
-function selectRandomTravelNumber() {
-    return Math.floor(Math.random() * travelArray.length);
-}
-// When the very first time the code loads what is the value of the array? Empty array, not Nothing.
-function renderTravel() {
-    while (randomArray.length < 3) {
-        // How do we get the randomArray to become false? Length must be higher number than 6. How do we change the length of the array? 
-
-        let random = selectRandomTravelNumber(); // 5
-        while (!randomArray.includes(random)) {
-            randomArray.push(random)
-        }
-        console.log(randomArray.length)
+function renderVacation(){
+  while (randomArray.length < 4) {
+    let randomNum = randomNumberGenerator();
+    if (!randomArray.includes(randomNum)) {
+      randomArray.push(randomNum);
     }
+  }
+  console.log(travelArray);
+
+  let imgOneIndex = randomArray.shift();
+  let imgTwoIndex = randomArray.shift();
+  let imgThreeIndex = randomArray.shift();
+  
+  vacationPicOne.src = travelArray[imgOneIndex].src;
+  vacationPicOne.alt = travelArray[imgOneIndex].name;
+  travelArray[imgOneIndex].votes++;
+  vacationPicTwo.src = travelArray[imgTwoIndex].src;
+  vacationPicTwo.alt = travelArray[imgTwoIndex].name;
+  travelArray[imgTwoIndex].votes++;
+  vacationPicThree.src = travelArray[imgThreeIndex].src;
+  vacationPicThree.alt = travelArray[imgThreeIndex].name;
+  travelArray[imgThreeIndex].votes++;
 }
-console.log(travelArray)
+
+
+
+// function selectRandomTravelNumber() {
+//   let randomNum;
+//   do {
+//     randomNum = Math.floor(Math.random() * travelArray.length);
+//   } while (travelArray.includes(randomNum));
+//   return randomNum;
+// }
+// function selectRandomTravelNumber() {
+//     return Math.floor(Math.random() * travelArray.length);
+// }
+// When the very first time the code loads what is the value of the array? Empty array, not Nothing.
 
 
 makeTravelArray();
-
-renderTravel();
+renderVacation ();
