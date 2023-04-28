fac58e39f5c0697ba747abb6b18bd12c0896bf95
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-04-28 01:44:09 -0400

    FIXED EVERYTHING

--- js/results.js
+++ js/results.js
@@ -1,4 +1,4 @@
-‘use strict’;
+'use strict';
 // let resultsVote = 7;
 // let productIndexArray = [];
 // let explore = new Travel ();
@@ -12,31 +12,20 @@ Highest votes are
 if statement helper
 */
 let travelArray = [];
-let savedArray = localStorage.getItem(‘travelArray’);
+let savedArray = localStorage.getItem('travelArray');
 if (savedArray) {
   travelArray = JSON.parse(savedArray);
 }
 let currentHighest = 0;
 let highestVotesGetter = {};
 for (let i = 0; i < travelArray.length; i++) {
-<<<<<<< HEAD
-    if (travelArray[i].votes > currentHighest) {
-        currentHighest = travelArray[i].votes;
-        highestVotesGetter = travelArray[i];
-    }
-}
-console.log(highestVotesGetter.src);
-document.getElementById('resultsImg').src = highestVotesGetter.src;
-=======
+
   if (travelArray[i].votes > currentHighest) {
     currentHighest = travelArray[i].votes;
     highestVotesGetter = travelArray[i];
   }
 }
 console.log(highestVotesGetter.src);
-document.getElementById(‘resultsImg’).src = highestVotesGetter.src;
-// 3:30 work on presentation 4pm meeting
-
->>>>>>> 0d0d92e44cd8edc9a9ee2f983adffb1ec9058373
+document.getElementById('resultsImg').src = highestVotesGetter.src;
 
 
