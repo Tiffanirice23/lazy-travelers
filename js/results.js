fd7e9bf6c03b0dadb4c26a245f473a3b8e731e2c
Arthur Franciso <arthur.b.francisco@gmail.com>
2023-04-28 09:08:32 +0700

    update

--- js/results.js
+++ js/results.js
@@ -1,4 +1,4 @@
-'use strict';
+‘use strict’;
 // let resultsVote = 7;
 // let productIndexArray = [];
 // let explore = new Travel ();
@@ -8,30 +8,25 @@
 /* view results function
 takes the voting data and find the array item with the highest votes
 for loop i = 0 <
-
 Highest votes are
 if statement helper
 */
 let travelArray = [];
-let savedArray = localStorage.getItem('travelArray');
+let savedArray = localStorage.getItem(‘travelArray’);
 if (savedArray) {
-
   travelArray = JSON.parse(savedArray);
 }
-
 let currentHighest = 0;
-function mostVotes() {
-  for (let i = 0; i < travelArray.length; i++) {
-    if (travelArray[i].votes > currentHighest) {
-      currentHighest = travelArray[i].votes;
-      console.log(travelArray[i]);
-      return travelArray[i].src;
-    }
+let highestVotesGetter = {};
+for (let i = 0; i < travelArray.length; i++) {
+  if (travelArray[i].votes > currentHighest) {
+    currentHighest = travelArray[i].votes;
+    highestVotesGetter = travelArray[i];
   }
 }
+console.log(highestVotesGetter.src);
+document.getElementById(‘resultsImg’).src = highestVotesGetter.src;
+// 3:30 work on presentation 4pm meeting
 
-mostVotes();
 
 
-
-// 3:30 work on presentation 4pm meeting 
